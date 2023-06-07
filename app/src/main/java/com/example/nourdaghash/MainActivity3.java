848c7bc5711017bb0f83e9f01b225ae79a1e7267
daghashnour28 <daghashnour28@gmail.com>
2023-06-08 01:27:06 +0300

    gfggty

--- app/src/main/java/com/example/nourdaghash/MainActivity3.java
+++ app/src/main/java/com/example/nourdaghash/MainActivity3.java
@@ -2,7 +2,11 @@ package com.example.nourdaghash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+
+         textView=(TextView)findViewById(R.id.textCakes)
+                 textView.setOnClickListner(new View.OnClickListener()) {
+@Override
+        public void onClick(View V)  {
+    Intent intent=new Intent(MainActivity3.this,MainActivity2.class)
+            startActivity(intent);
+
+                Toast.makeText(MainActivity3.this,"CAKES CATEGORY",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/nourdaghash/cakes_activity.java
@@ -0,0 +1,14 @@
+package com.example.nourdaghash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cakes_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cakes);
+    }
+}
