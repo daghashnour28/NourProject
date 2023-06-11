c10314be8a76d805b171cff222970d75193032c0
daghashnour28 <daghashnour28@gmail.com>
2023-06-12 01:01:27 +0300

    gfggtyjj

---
+++ app/src/main/java/com/example/nourdaghash/choclate_cake.java
@@ -0,0 +1,14 @@
+package com.example.nourdaghash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class choclate_cake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choclate_cake);
+    }
+}
---
+++ app/src/main/java/com/example/nourdaghash/noBake_CheeseCake.java
@@ -0,0 +1,14 @@
+package com.example.nourdaghash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class noBake_CheeseCake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_bake_cheese_cake);
+    }
+}
