e52bd5bb2b3e28b7761e1afcd4e182670c9ffa3c
daghashnour28 <daghashnour28@gmail.com>
2023-06-12 11:51:29 +0300

    gfggtyjj

--- app/src/main/java/com/example/nourdaghash/cupcakes_activity.java
+++ app/src/main/java/com/example/nourdaghash/cupcakes_activity.java
@@ -11,4 +11,23 @@ public class cupcakes_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cupcakes);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewSignInTitle = findViewById(R.id.textViewSignInTitle);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        buttonSignIn = findViewById(R.id.button);
+
+        preferences = getSharedPreferences("UserInfo", 0);
+    }
+
+
+
+
+
+
 }
